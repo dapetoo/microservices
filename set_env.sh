f7f8520da74cbe8260bb61db82e9e82c0898260c
dapetoo <dapetoo@gmail.com>
2021-09-01 16:03:44 +0100

    frontend updated

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+setx POSTGRES_USERNAME postgres
+setx POSTGRES_PASSWORD abcd1234
+setx POSTGRES_HOST mypostgres-database-1.ch4a9dhlinpw.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB postgres
+setx AWS_BUCKET test-nd9990-dev-wc
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET hello
+setx URL http://localhost:8100
